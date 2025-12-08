f6c856e3237a4fb636e1d986b0eb7595d7106586
quehoang25112006-source <quehoang25112006@gmail.com>
2025-12-08 10:42:10 +0700

    Add files via upload

---
+++ BAI4/4.11.py
@@ -0,0 +1,9 @@
+print("Họ và tên :HOANG VAN QUE")
+print("MSSV:2457520221610045")
+print("#####################")
+###########################
+ds = [] # "tạo list rỗng"
+ds.append("abc") # "thêm phần tử vào list"
+
+for ch in ds:
+    print(ch)
